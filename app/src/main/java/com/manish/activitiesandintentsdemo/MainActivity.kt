74c8a54d3660beda771c8f77aac9e5470bc7a2d7
Manish Negi <manish.negi@tothenew.com>
2021-09-29 18:11:06 +0530

    intent examples added

--- app/src/main/java/com/manish/activitiesandintentsdemo/MainActivity.kt
+++ app/src/main/java/com/manish/activitiesandintentsdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.manish.activitiesandintentsdemo
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -35,12 +36,16 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btn_launch_activity).setOnClickListener {
-            launchActivityB()
+
+        findViewById<Button>(R.id.btn_launch_implicit_intent).setOnClickListener {
+            launchImplicitIntent()
         }
 
-        findViewById<Button>(R.id.btn_launch_activity_for_result).setOnClickListener {
-            startForResult.launch(Intent(this, ActivityB::class.java))
+        findViewById<Button>(R.id.btn_launch_explicit_intent).setOnClickListener {
+            launchActivityB()
+        }
+        findViewById<Button>(R.id.btn_launch_implicit_intent_with_chooser).setOnClickListener {
+            launchImplicitIntentWithChooser()
         }
     }
 
@@ -95,5 +100,42 @@ class MainActivity : AppCompatActivity() {
         startActivity(intentForActivityB)
     }
 
+    private fun launchImplicitIntent() {
+        val implicitIntent = Intent().apply {
+            action = Intent.ACTION_SEND  //ACTION TO BE PERFORMED
+            putExtra(
+                Intent.EXTRA_TEXT,
+                "This is the text placed as an extra within the intent object"
+            )
+            type = "text/plain"   // MIME TYPE
+        }
+
+        try {
+            startActivity(implicitIntent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                this,
+                "No activity can handle the action of the intent",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun launchImplicitIntentWithChooser() {
+        val implicitIntent = Intent().apply {
+            action = Intent.ACTION_SEND  //ACTION TO BE PERFORMED
+            putExtra(
+                Intent.EXTRA_TEXT,
+                "This is the text placed as an extra within the intent object"
+            )
+            type = "text/*"   // MIME TYPE
+        }
+        val chooser = Intent.createChooser(implicitIntent, "This is the chooser window title...")
+
+        if (implicitIntent.resolveActivity(packageManager) != null) {
+            startActivity(chooser)
+        }
+
+    }
 }
 
